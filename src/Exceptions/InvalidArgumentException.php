96fbeaa65daa4360697146a79cf7ab040a52680a
liaochao2011 <76660421@qq.com>
2018-09-04 03:07:32 +0000

    Apply fixes from StyleCI

--- src/Exceptions/InvalidArgumentException.php
+++ src/Exceptions/InvalidArgumentException.php
@@ -1,7 +1,16 @@
 <?php
+
+/*
+ * This file is part of the alanliao/weather.
+ *
+ * (c) alanliao <76660421@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Alanliao\Weather\Exceptions;
 
 class InvalidArgumentException extends Exception
 {
-
 }
--- src/Weather.php
+++ src/Weather.php
@@ -1,4 +1,14 @@
 <?php
+
+/*
+ * This file is part of the alanliao/weather.
+ *
+ * (c) alanliao <76660421@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Alanliao\Weather;
 
 use Alanliao\Weather\Exceptions\HttpException;
@@ -8,7 +18,9 @@ use GuzzleHttp\Client;
 class Weather
 {
     protected $key;
+
     protected $guzzleOptions = [];
+
     public function __construct($key)
     {
         $this->key = $key;
@@ -26,33 +38,33 @@ class Weather
 
     public function getWeather($city, $type = 'live', $format = 'json')
     {
-        $url   = 'https://restapi.amap.com/v3/weather/weatherInfo';
+        $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
         $types = [
-            "live"     => "base",
-            "forecast" => "all",
+            'live' => 'base',
+            'forecast' => 'all',
         ];
         if (!\in_array(\strtolower($format), ['json', 'xml'])) {
-            throw new InvalidArgumentException('Invalid response format: ' . $format);
+            throw new InvalidArgumentException('Invalid response format: '.$format);
         }
         if (!\array_key_exists(\strtolower($type), $types)) {
-            throw new InvalidArgumentException('Invalid type value(live/forecast): ' . $type);
+            throw new InvalidArgumentException('Invalid type value(live/forecast): '.$type);
         }
         $query = array_filter([
-            'key'        => $this->key,
-            'city'       => $city,
-            'output'     => strtolower($format),
+            'key' => $this->key,
+            'city' => $city,
+            'output' => strtolower($format),
             'extensions' => strtolower($types[$type]),
         ]);
-        try {
 
+        try {
             $response = $this->getHttpClient()->get($url, [
                 'query' => $query,
             ])->getBody()->getContents();
-            return $format === 'json' ? \json_decode($response, true) : $response;
+
+            return 'json' === $format ? \json_decode($response, true) : $response;
         } catch (\Exception $e) {
             throw new HttpException($e->getMessage(), $e->getCode(), $e);
         }
-
     }
 
     public function getLiveWeather($city, $format = 'json')
@@ -64,5 +76,4 @@ class Weather
     {
         return $this->getWeather($city, 'forecast', $format);
     }
-
 }
